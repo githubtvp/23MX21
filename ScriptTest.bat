af4ba26b2ebafb9ec48c8b2c25e4d481fc90ad46
githubtvp <tvp1640@gmail.com>
2024-02-05 00:17:52 +0530

    Saving latest Updates with script

---
+++ ScriptTest.bat
@@ -0,0 +1,28 @@
+@ECHO off
+:: Check the setup_database.bat file in CIT-181(OracleBasic) db_setup directory
+
+mode con:cols=120 lines=150
+cd C:\Users\Varadha\Desktop\23MX21
+call gitScript.bat
+cd C:\Users\Varadha\Desktop\23MX22
+call gitScript.bat
+cd C:\Users\Varadha\Desktop\23MX22
+call gitScript.bat
+cd C:\Users\Varadha\Desktop\23MX23
+call gitScript.bat
+cd C:\Users\Varadha\Desktop\23MX24
+call gitScript.bat
+cd C:\Users\Varadha\Desktop\23MX26
+call gitScript.bat
+cd C:\Users\Varadha\Desktop\23MX27
+call gitScript.bat
+cd C:\Users\Varadha\Desktop\23MX28
+call gitScript.bat
+cd C:\Users\Varadha\Desktop\23MXAG
+call gitScript.bat
+::cmd - holds the screen display till exited mannually
+::cmd  
+::exit
+::ECHO
+:: Display 'press any key to continue' message
+::PAUSE
